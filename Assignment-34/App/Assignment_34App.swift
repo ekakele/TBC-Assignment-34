b5f3803792295887c231382d4509217cf1b1b2c8
ekakele <ekaakelenjeridze@gmail.com>
2024-01-02 14:44:28 +0400

    dd gitignore & organize folder strucutre

---
+++ Assignment-34/App/Assignment_34App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_34App.swift
+//  Assignment-34
+//
+//  Created by Eka Kelenjeridze on 02.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_34App: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Assignment-34/Screens/MainScreen/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  Assignment-34
+//
+//  Created by Eka Kelenjeridze on 02.01.24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
